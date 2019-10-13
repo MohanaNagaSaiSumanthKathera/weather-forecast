722e2fbf023d8848ddb1224685f809d325f6cd7a
Sumanth Kathera <Sumanth.Kathera@arm.com>
2019-10-13 12:15:10 +0100

    configured header and footer as partials

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
+//defining paths
 // to fix absolute path for folder "public" where the static content is loaded
+//defining default views path to another
 const publicDirectoryPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../templates/views');
+const partialPath = path.join(__dirname,'../templates/partials');
 
-//configuring default views path to another
-const viewsPath = path.join(__dirname,'../templates');
-
-app.use(express.static(publicDirectoryPath));
-
-
+//configuring view engine, views , partials
 //handle bars - hbs --> template engine to implement dynamic websites. renders views folder by default
 app.set('view engine', 'hbs');
 app.set('views',viewsPath);
+hbs.registerPartials(partialPath);
+
+//setting up the static direcory to serve.
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req,res)=>{
     res.render('index',{
@@ -24,11 +28,17 @@ app.get('', (req,res)=>{
 });
 
 app.get('/about',(req,res)=>{
-    res.render('about');
+    res.render('about',{
+        title : 'About Page',
+        name : 'Sumanth'
+    });
 });
 
 app.get('/help',(req,res)=>{
-    res.render('help');
+    res.render('help',{
+        title : 'Help Page',
+        name : 'Sumanth'
+    });
 });
 
 app.get('/weather',(req,res)=>{
