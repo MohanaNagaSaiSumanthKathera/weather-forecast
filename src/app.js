28647e22f4479ca8cf36f1a61c99e3966395aa0a
Sumanth Kathera <Sumanth.Kathera@arm.com>
2019-10-13 11:33:40 +0100

    customized the folder name for hbs view engine

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ const path = require('path');
 
 const app = express();
 
+// to fix absolute path for folder "public" where the static content is loaded
 const publicDirectoryPath = path.join(__dirname,'../public');
+
+//configuring default views path to another
+const viewsPath = path.join(__dirname,'../templates');
 
 app.use(express.static(publicDirectoryPath));
 
+
 //handle bars - hbs --> template engine to implement dynamic websites. renders views folder by default
 app.set('view engine', 'hbs');
+app.set('views',viewsPath);
 
 app.get('', (req,res)=>{
     res.render('index',{
